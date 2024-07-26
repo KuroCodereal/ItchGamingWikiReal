73dbc6b37c534f1ace66c59fd489bda6e8137b56
hoandv2003002@gmail.com <hoandv2003002@gmail.com>
2024-07-26 17:13:52 +0700

    IGWZipFile not complete

---
+++ IGW.Files/IGWFileContentType.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGW.Files
+{
+    public class IGWFileContentType
+    {
+        //DOC = application/msword
+        public static string DOC = "application/msword";
+
+        //DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        public static string DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+        //XLS = "application/vnd.ms-excel";
+        public static string XLS = "application/vnd.ms-excel";
+
+        //XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        public static string XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        //PPT = "application/vnd.ms-powerpoint"
+        public static string PPT = "application/vnd.ms-powerpoint";
+
+        //TXT = "text/plain";
+        public static string TXT = "text/plain";
+
+        //PDF = "application/PDF";
+        public static string PDF = "application/pdf";
+
+        //IMG = "image/*";
+        public static string IMG = "image/*";
+
+        //JPEG = "image/jpeg";
+        public static string JPEG = "image/jpeg";
+
+        //JPG = "image/jpg";
+        public static string JPG = "image/jpg";
+
+        //PNG = "image/png";
+        public static string PNG = "image/png";
+
+        //DEFAULT = "multipart/form-data"
+        public static string DEFAULT = "multipart/form-data";
+
+        public static Dictionary<string, string> Dictionary = new Dictionary<string, string>()
+        {
+            { "DOC", "application/msword"},
+            { "DOCX", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            { "XLS", "application/vnd.ms-excel"},
+            { "XLSX", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            { "PPT", "application/vnd.ms-powerpoint"},
+            { "TXT", "text/plain"},
+            { "PDF", "application/pdf"},
+            { "IMG","image/*"},
+            { "JPEG","image/jpeg"},
+            { "JPG","image/jpg"},
+            { "PNG","image/png"},
+            { "DEFAULT","multipart/form-data" }
+        };
+
+        //Ten file full
+        public static string Get(string FileName)
+        {
+            string ExtFile = System.IO.Path.GetExtension(FileName).Substring(1).ToUpper();
+            if (!Dictionary.ContainsKey(ExtFile)) return DEFAULT;
+            return Dictionary[ExtFile];
+        }
+
+    }
+}
---
+++ IGW.Files/IGWZipFile.cs
@@ -0,0 +1,28 @@
+﻿using ItchGamingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGW.Files
+{
+    public class IGWZipFile
+    {
+        //Nen cac file thanh .zip
+        //FormPath: Thu muc chua cac file can nen
+        //ToZipPath: thu muc chua file sau khi nen
+        //ZipFileName: ten file sau khi nen
+        public IGWMessage AddToZipFile(string FromPath, string ToZipPath, string ZipFilename = "")
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(FromPath) || string.IsNullOrEmpty(ToZipPath))
+                {
+                    return new IGWMessage(false, null, "FromPath and ToZipPath are invalid");
+                }
+                //ZipFilename = "IGWZipFile" + IGWCommom
+            }
+        }
+    }
+}
